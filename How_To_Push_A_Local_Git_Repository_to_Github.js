2ccd62956eda017ad5936ee284f9f39d06761716
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-04 10:07:39 -0700

    change

---
+++ How_To_Push_A_Local_Git_Repository_to_Github.js
@@ -0,0 +1,18 @@
+// First things first GITHUB and GIT are NOT the same thing.
+
+// Git Command:
+/*
+(Other than the link all of this is GIT code, not GITHUB.)
+git remote add origin https://github.com/jadecobra835/How_To_Use_Github.git
+^(This adds the "fetch" remote and the "push" remote)
+  git branch -M main
+  ^(This will create the master branch)
+  git push -u origin main
+  ^(This will push your code to the master branch)
+*/
+
+// Other Git Commands
+/*
+git remote -v
+^(This will show you the "fetch" remote and the "push" remote)
+*/
