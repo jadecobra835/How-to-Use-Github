a78d21d27bf432b579950eaec5e5a36278e1568d
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-04 09:49:37 -0700

    Initial commit

---
+++ How_to_Create_a_Local_Git_Repository.js
@@ -0,0 +1,23 @@
+// All of this is done in the terminal.
+// Git Basics: (Super common Git commands);
+// Non Git Commands:
+// mkdir (name of your new directory)
+// cd (name of a child directory of the one you are currently in)
+// code (name of a new file)
+// dir (shows the non hidden files in your directory)
+// dir -Hidden (Shows only the hidden files in your directory)
+
+// Git Commands
+/*
+git init
+^(This will make a new repository and give you the path.)
+
+git status
+^(This will tell you the state of your directory)
+
+git add .
+^(This will add all untracked files)
+
+git add (path)
+^(This will add everything inside of the directory.)
+*/
