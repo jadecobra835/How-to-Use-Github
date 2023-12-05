ae7e0698ddae8c002ce88b3fdf851c426387fbf2
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-05 10:16:14 -0700

    Adding the .gitignore guide

---
+++ How_to_Hide_Files_and Directories_from_Git_with_gitignore.js
@@ -0,0 +1,14 @@
+// create a .gitinore file
+// Add the name of your secret file to it and possibly the .gitignore as well.
+// By adding a / to the end of a name in the gitignore (ex node_modules/) it will also ignore all the files inside of it.
+
+
+
+// Git Commands
+/*
+git status
+^(Will tell you if you have an unpushed git message or untracked files.)
+
+git rm -r --cached .
+^(This will remove all of the cached changes on your machine. You should only really do this if you are having an issue where the .gitignore isn't working) 
+*/
