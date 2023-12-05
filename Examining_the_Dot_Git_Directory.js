47b8402fffad8325c2f833f434665b42c348670f
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-05 09:56:48 -0700

    Adding a guide to the .git file

---
+++ Examining_the_Dot_Git_Directory.js
@@ -0,0 +1,41 @@
+// First turn your hidden files on in the Files:Exclude setting.
+// Now we are going to explor the .git directory.
+
+// HOOKS: A tool that can be run at a specific time. Ex: Pre-push will occur before the push
+
+// INFO: exclude: Shows everything that will be excluded from git messages.
+
+// LOGS:
+
+// logs: HEAD: stores all of the git logs (commit messages.)
+
+// logs: refs: heads: main: Stores the current head location
+
+// logs: refs: remotes/origin: main: Stores the remote origin main.
+
+// OBJECTS: You will never use these. They are just objects in memory.
+
+// REFS: Stores all of the references
+
+    // refs: heads: main: Stores the current git id
+
+    // refs: remotes.origin: main: Stores the remote origin id
+
+    // regs: tags: Will store whatever tags you add to the repository.
+
+// COMMIT_EDITMSG: Stores the most recent commit message and allows you to change it.
+
+// CONFIG: Shows a bunch of data including the URL for the remote and the merge variables.
+
+// DESCRIPTION: If you wanted to add a description to your git repository this is how you would do it.
+
+// HEAD: Again storing the reference to the git id.
+
+// index: This is your commit message.
+
+
+// Git Commands
+/*
+git log
+^(This will show every commit  message that you have made inside of this directory.)
+*/
