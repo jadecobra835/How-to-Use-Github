98115100149f442cf8d352749a9403d1e9a8e4b1
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-06 09:24:33 -0700

    Adding the git pull guide

---
+++ How_to_Pull_files.js
@@ -0,0 +1,7 @@
+// If another dev makes a change to the project you are working on and pushes it to the git repository,
+// It won't automaticly populate in your on system code, to do that you need to pull the updated code into your system.
+// This is how we do that.
+
+/*
+git pull
+*/
