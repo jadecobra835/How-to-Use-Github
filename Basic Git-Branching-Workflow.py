dff037c63795b8067586354d37a2506a8d4b81ea
xanderjensen <xanderjamesjensen@icloud.com>
2023-12-06 10:00:36 -0700

    Working with local Branches

---
+++ Basic Git-Branching-Workflow.py
@@ -0,0 +1,22 @@
+# First thing we are going to do is learn some commands.
+
+"""
+GIT COMMANDS IN THIS ORDER:
+git branch
+^(This will show the branches that exist on your LOCAl machine. Not on github)
+
+git checkout -b name-of-branch
+^(This will create a new branch and switch you into it.)
+
+# If you want to know what branch you are on just type git branch.
+# From here add something to the thing you want to edit
+
+git checkout name-of-branch
+^(This will switch you into or out of branches)
+
+# You will notice that the changes you made while in the side branch aren't populated on the main branch.
+
+git merge name-of-branch
+^(This will populate all of the changes you made to the master branch.)
+
+"""
---
+++ Why-Use-Branches.py
@@ -0,0 +1,5 @@
+# Branches are usualy used when the change you need to implement isn't just a quick fix.
+# If you don't want to make all of the changes necessary on the live website, that's when you want to use branches.
+# Branches are usually called Feature Branches becuase of this.
+# This allows everyone to have the working version of the code and you still have your feature to work on.
+# You can then merge this to the main branch and they can simply update their code with a git pull.
